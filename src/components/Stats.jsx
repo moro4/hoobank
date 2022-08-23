0edf219152d6e52f9c2209f077bd2d596cd6d941
zepeersGily <>
2022-08-23 18:04:19 +0200

    implemented stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import {stats} from '../constants';
+import styles from '../style';
 
 function Stats() {
    return (
-      <div>Stats</div>
+      <section className={styles.flexCenter + 'flex-row flex-wrap\
+         sm:mb-20 mb-6'}
+
+      >
+         {stats.map((stats) => (
+            <>
+            <div key={stats.id} className='flex-1 flex justify-start
+               items-center flex-row ml-3'
+            >
+               <h4 className='font-poppins font-semibold xs:text-[40px]
+                  text-[40px] xs:leading-[53px] leading-[43px] text-white'
+               >
+                  {stats.value}
+               </h4>
+               <p className='font-poppins font-normal xs:text-[20px]
+                  text-[15px] xs:leading-[26px] leading-[21px]
+                  uppercase ml-3 text-gradient'
+               >
+                  {stats.title}
+               </p>
+            </div>
+            </>
+         ))}
+      </section>
    )
 }
 
