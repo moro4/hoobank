aa242572e7726819ffaa92febe4e6adeeff2df85
zepeersGily <>
2022-08-25 11:19:56 +0200

    implemented card section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Button({styles}) {
    return (
       <button type='button' className={styles + ' py-4 px-6 bg-blue-gradient \
-         font-poppins rounded-[10px] font-medium text-[18px] text-primary outline-none'}
+         font-poppins rounded-[10px] font-medium text-[18px]\
+         text-primary outline-none'}
       >
          Get Started!
       </button>
